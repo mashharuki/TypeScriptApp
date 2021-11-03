9f9b988e2ea0a1024500ad445086cda4066c822a
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-11-03 21:36:06 +0900

    追加実装

--- src/class.js
+++ src/class.js
@@ -3,8 +3,13 @@
  */
 // Personクラス
 var Person = /** @class */ (function () {
-    function Person() {
+    function Person(name, mail, age) {
+        if (mail === void 0) { mail = 'no-mail'; }
+        if (age === void 0) { age = -1; }
         this.name = 'no-name';
+        this.name = name;
+        this.mail = mail;
+        this.age = age;
     }
     Person.prototype.print = function () {
         var ml = this.mail ? this.mail : 'no-mail';
@@ -13,8 +18,13 @@ var Person = /** @class */ (function () {
     };
     return Person;
 }());
-var taro = new Person();
-taro.name = 'taro';
-taro.mail = 'taro@test.com';
-taro.age = 39;
+var taro = new Person('taro', 'taro@test.com', 39);
+var hanako = new Person('hanako', 'hanako@test.com', 20);
+var sachiko = new Person('sachiko', 'sachiko@test.com', 27);
 taro.print();
+hanako.print();
+sachiko.print();
+console.log(taro instanceof Person === hanako instanceof Person === true);
+console.log(taro.constructor.name);
+console.log(hanako.constructor.name);
+console.log(Person.name);
--- src/class.ts
+++ src/class.ts
@@ -7,6 +7,13 @@ class Person {
     name:string = 'no-name'
     mail?:string
     age?:number
+
+    constructor(name:string, mail:string = 'no-mail', age:number = -1) {
+        this.name = name
+        this.mail = mail
+        this.age = age
+    }
+
     print():void {
         const ml:string = this.mail ? this.mail : 'no-mail'
         const ag:number = this.age ? this.age : -1
@@ -14,9 +21,15 @@ class Person {
     }
 }
 
-const taro = new Person()
-taro.name = 'taro'
-taro.mail = 'taro@test.com'
-taro.age = 39
+const taro = new Person('taro', 'taro@test.com', 39)
+const hanako = new Person('hanako', 'hanako@test.com', 20)
+const sachiko = new Person('sachiko', 'sachiko@test.com', 27)
 
 taro.print()
+hanako.print()
+sachiko.print()
+
+console.log(taro instanceof Person === hanako instanceof Person === true)
+console.log(taro.constructor.name)
+console.log(hanako.constructor.name)
+console.log(Person.name)
