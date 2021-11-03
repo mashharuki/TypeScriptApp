24fcce6b2f3e37590a74e545db440cd7aa369e73
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-11-03 21:46:53 +0900

    継承をサンプル実装

--- src/class.js
+++ src/class.js
@@ -1,6 +1,21 @@
 /**
  * クラスの利用をマスターするためのファイル
  */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // Personクラス
 var Person = /** @class */ (function () {
     function Person(name, mail, age) {
@@ -28,3 +43,37 @@ console.log(taro instanceof Person === hanako instanceof Person === true);
 console.log(taro.constructor.name);
 console.log(hanako.constructor.name);
 console.log(Person.name);
+// School列挙型変数
+var School;
+(function (School) {
+    School["junior"] = "junior";
+    School["juniorHigh"] = "juniorHigh";
+    School["high"] = "high";
+    School["other"] = "other";
+})(School || (School = {}));
+// Personクラスを継承したStudentクラスを作成する。
+var Student = /** @class */ (function (_super) {
+    __extends(Student, _super);
+    function Student(name, school, grade) {
+        var _this = _super.call(this, name) || this;
+        _this.school = school;
+        _this.grade = grade;
+        switch (school) {
+            case School.junior:
+                _this.age = 6 + _this.grade;
+                break;
+            case School.juniorHigh:
+                _this.age = 12 + _this.grade;
+                break;
+            case School.high:
+                _this.age = 15 + _this.grade;
+                break;
+            default:
+                _this.age = -1;
+        }
+        return _this;
+    }
+    return Student;
+}(Person));
+var hanako2 = new Student('hanako2', School.high, 2);
+hanako2.print();
--- src/class.ts
+++ src/class.ts
@@ -33,3 +33,42 @@ console.log(taro instanceof Person === hanako instanceof Person === true)
 console.log(taro.constructor.name)
 console.log(hanako.constructor.name)
 console.log(Person.name)
+
+// School列挙型変数
+enum School {
+    junior = 'junior',
+    juniorHigh = 'juniorHigh',
+    high = 'high',
+    other = 'other'
+}
+
+// Personクラスを継承したStudentクラスを作成する。
+class Student extends Person {
+    school ? : School
+    grade ? : number
+
+    constructor(name: string, school: School, grade: number) {
+        // スーパークラスであるPersonのコンストラクターを呼び出している。
+        super(name)
+        this.school = school
+        this.grade = grade
+
+        switch(school) {
+            case School.junior:
+                this.age = 6 + this.grade
+                break
+            case School.juniorHigh:
+                this.age = 12 + this.grade
+                break
+            case School.high:
+                this.age = 15 + this.grade
+                break
+            default:
+                this.age = -1
+        }
+    }
+}
+
+const hanako2 = new Student('hanako2', School.high, 2)
+hanako2.print()
+
